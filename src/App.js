c9c2520e285aaad5732c28f0e6c8b82ee7e9b5e3
sakalmon <sakalathmon@gmail.com>
2023-02-06 20:12:49 +1000

    Converted App to class component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.scss';
+import React from 'react';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import PopularResorts from './components/PopularResorts';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Navigation />
-      <PopularResorts />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Navigation />
+        <PopularResorts />
+      </div>
+    );
+  }
 }
 
 export default App;
