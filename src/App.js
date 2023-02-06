71e5c00d63f78aec64ff558674f176b81e0b4ee2
sakalmon <sakalathmon@gmail.com>
2023-02-06 20:02:02 +1000

    Implemented sass

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import PopularResorts from './components/PopularResorts';
