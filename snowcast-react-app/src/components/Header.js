9162f0df217a2d7510f4d1c314ddccb8dcb5f212
sakalmon <sakalathmon@gmail.com>
2023-02-07 15:19:20 +1000

    Styled popular resorts page

--- snowcast-react-app/src/components/Header.js
+++ snowcast-react-app/src/components/Header.js
@@ -1,8 +1,8 @@
 function Header() {
   return (
     <div className="Header">
+      <img className="logo" src="https://cdn-icons-png.flaticon.com/512/9112/9112733.png" alt="logo" />
       <h1 className="title">Snowcast</h1>
-      <img src="#" alt="logo" />
     </div>
   );
 }
--- snowcast-react-app/src/components/Resort.js
+++ snowcast-react-app/src/components/Resort.js
@@ -3,7 +3,7 @@ function Resort({ resortName, country, snowFallToday, currentTemp }) {
     <div className="Resort">
         <p className="name">{resortName}</p>
         <p className="country">{country}</p>
-        <p className="snowfall-today">Forcasted snowfall today: {snowFallToday}mm</p>
+        <p className="snowfall-today">Snowfall (next 24h): {snowFallToday}mm</p>
         <p className="current-temp">Current Temperature: {currentTemp} &deg;C</p>
     </div>
   );
