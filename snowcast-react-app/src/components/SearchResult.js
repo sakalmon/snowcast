1916ba41d5e59c01ab10c896c974ed4b856a3334
sakalmon <sakalathmon@gmail.com>
2023-02-09 00:28:09 +1000

    Fixed issue with today's snowfall not displaying for search results

--- snowcast-react-app/src/components/SearchResult.js
+++ snowcast-react-app/src/components/SearchResult.js
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
 
 function SearchResult(props) {
-  const { resortName, country } = props.result;
-  const resortObj = props.result;
+  const { resortName, country, currentTemp, hourlySnowFall, iconCode, snowFall } = props.result;
+  const resortObj = { resortName, country, currentTemp, hourlySnowFall, iconCode }
+  resortObj.snowFallToday = snowFall;
+  console.log('resortObj')
+  console.log(resortObj)
 
   return (
     <div className="SearchResult">
